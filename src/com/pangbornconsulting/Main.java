6339deab38196ae9e4ff2a705fa1e95cd021d97d
Thomas S. Pangborn <thomas.s.pangborn@gmail.com>
2015-03-22 22:42:26 -0400

    Initial files.

---
+++ src/com/pangbornconsulting/Main.java
@@ -0,0 +1,9 @@
+package com.pangbornconsulting;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
